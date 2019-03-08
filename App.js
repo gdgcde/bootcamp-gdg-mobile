6c36f320d2ce5822e961e580b6cb206f508c4639
Ivan Caceres <ivansfy@gmail.com>
2019-03-08 08:25:35 -0300

    Case of use of the states
    Signed-off-by: Ivan Caceres <ivansfy@gmail.com>

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      stateA: "stateA",
+      stateB: "stateB",
+      stateC: 1
+    };
+    setInterval(() => {
+      this.setState({ stateC: this.state.stateC + 1 });
+    }, 1000);
+  }
+
+  switchValues = () => {
+    const { stateA, stateB } = this.state;
+    this.setState({ stateA: stateB, stateB: stateA });
+  };
+
   render() {
+    console.log("States", this.state);
+    const { stateA, stateB, stateC } = this.state;
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>This is the state A: {stateA}!</Text>
+        <Text>This is the state B: {stateB}!</Text>
+        <Text>This is the state C: {stateC}!</Text>
+        <Button title="Switch states" onPress={this.switchValues} />
       </View>
     );
   }
@@ -14,8 +37,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
