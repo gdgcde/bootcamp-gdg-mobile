4c96cb97cf0bd4086bc73e35cb1f7d1e2345f96b
Ivan Caceres <ivansfy@gmail.com>
2019-03-08 09:34:58 -0300

    Styles for text
    Signed-off-by: Ivan Caceres <ivansfy@gmail.com>

--- src/components/Component1/Component1.js
+++ src/components/Component1/Component1.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default class Component1 extends Component {
   constructor(props) {
@@ -10,10 +10,18 @@ export default class Component1 extends Component {
     const { name, state } = this.props;
     return (
       <View>
-        <Text>
+        <Text style={styles.text}>
           This is the state {name}: {state}!
         </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 20
+  }
+});
